96fd30495ec2d4a5775f21b783fca6d9be670006
HigorCarv <higorcarvalho502@gmail.com>
2024-08-30 14:11:30 -0300

    first commit

---
+++ index.py
@@ -0,0 +1,7 @@
+Nome = 'Higor'
+Idade = 18
+Cidade = 'Imperatriz'
+def  informacoes_pessoais():
+     print(f'Meu nome é {Nome}, minha idade é {Idade}, minha ciade natal é {Cidade}')
+
+informacoes_pessoais()
